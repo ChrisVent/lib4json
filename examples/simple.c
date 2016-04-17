0b7c7433ea2b8e55b943b72246e96e6d6591b56a
ChrisVent <venturachristopher98@gmail.com>
2016-04-17 14:28:39 -0400

    Refactoring

--- examples/simple.c
+++ examples/simple.c
@@ -1,33 +1,18 @@
-<<<<<<< HEAD
 #include "../lib4json.c"
 
 int main (int args, const char *argv[]){
     // #    create the json string
     const char* json = "{\"userID\":1,\"username\":\"Admin\",\"pass\":\"Lorem123\",\"name\":\"Christian Brown\",\"admin\":true,\"array\":[\"julia\",\"robert\"],\"Object\":{\"julia\":\"robert\"}}";
-=======
-#include "../jsonparser.c"
-
-int main (int args, const char *argv[]){
-    // #    create the json string
-    const char* json = "{\"userID\":1,\"username\":\"Admin\",\"pass\":\"Lorem123\",\"name\":\"Christian Brown\",\"admin\":true}";
->>>>>>> 90d9987f42c9a517a1c424b91bb6cab784484b0f
     // #    create json object and parse the string
     jsonObject myJson = jsonParse(json);
     // #    print the json parsed
     printJsonParsed(myJson);
     // #    print the json stringed
     //printJsonString(myJson);
-<<<<<<< HEAD
     // #    save the values in array
     char** values = getJsonValues(myJson);
     int i;
     printf("\n====================================\n\n");
-=======
-
-    // #    save the values in array
-    char** values = getJsonValues(myJson);
-    int i;
->>>>>>> 90d9987f42c9a517a1c424b91bb6cab784484b0f
     for(i = 0 ; i < myJson.size ; i++){
         fprintf(stdout,"Value: %s\n",values[i]);
     }
--- lib4json.h
+++ lib4json.h
@@ -17,8 +17,6 @@ typedef enum
 }
 jsonType;
 
-
-<<<<<<< HEAD
 typedef enum{
     ERR_JSON_BROKEN = 1,
     ERR_JSON_MEMORY,
@@ -26,33 +24,22 @@ typedef enum{
 }
 jsonErr;
 
-=======
->>>>>>> 90d9987f42c9a517a1c424b91bb6cab784484b0f
 typedef struct
 jsonItem{
     int start;
     int end;
     jsonType type;
-<<<<<<< HEAD
     jsonItem* child;
     jsonItem* next;
     const char* key;
-=======
-    jsonItem* childs;
->>>>>>> 90d9987f42c9a517a1c424b91bb6cab784484b0f
 
 }
 jsonItem;
 
 typedef struct
 {
-<<<<<<< HEAD
     jsonItem* first_child;
     jsonItem* last_child;
-=======
-    jsonItem* values;
-    char** keys;
->>>>>>> 90d9987f42c9a517a1c424b91bb6cab784484b0f
     unsigned int size;
     const char* string;
 }
@@ -61,19 +48,12 @@ jsonObject;
 
 
 const char* getTypeJson(int type);
-<<<<<<< HEAD
 void fromJson(jsonObject *theJson);
-=======
-void toJson(jsonObject *theJson);
->>>>>>> 90d9987f42c9a517a1c424b91bb6cab784484b0f
 void printJsonString(jsonObject myJson);
 void jsonStringify(jsonObject *json);
 char** getJsonValues(jsonObject myJson);
 jsonObject jsonParse(const char* stringed);
 void printJsonParsed(jsonObject myJson);
-<<<<<<< HEAD
 void ArrayFromJSON(jsonObject *theJson);
 void ObjectFromJSON(jsonObject *theJson);
-=======
->>>>>>> 90d9987f42c9a517a1c424b91bb6cab784484b0f
 #endif
